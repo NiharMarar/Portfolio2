cb0e2e32dbef886ac08652d89028706c442c2464
GingerTC <72882312+GingerTC@users.noreply.github.com>
2020-11-17 09:51:09 -0800

    new routes to HelloSeries.html and FlaskSeries.html

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 def navbar():
     return render_template("base.html")
 
+@app.route('/Hello-Series')
+def HelloSeries():
+    return render_template("HelloSeries.html")
+
+@app.route('/Flask-Series')
+def FlaskSeries():
+    return render_template("FlaskSeries.html")
