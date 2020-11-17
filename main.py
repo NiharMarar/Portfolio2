aa88d4d22ba04f3e6c1b1b2855feadbb6098ebe3
Anthony <giustanthony@gmail.com>
2020-11-17 15:11:50 -0800

    More

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, url_for , render_template
 app = Flask(__name__)
 
 @app.route('/home')
+@app.route('/')
 def home():
     return render_template("homepage.html")
 
